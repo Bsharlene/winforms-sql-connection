0891c0d1487d2228d8b5ff2f414cd997b650a52b
sha_badilles <badillessharlene25@gmail.com>
2026-03-29 22:51:24 +0800

    Add ConnectionSql class for MySQL database connection

---
+++ TestWins/model/UpdateConnectionSql.cs
@@ -0,0 +1,24 @@
+namespace TestWins.Model;
+
+//dotnet add package MySql.Data
+using MySql.Data.MySqlClient;
+
+    public class ConnectionSql
+    {
+        private readonly string _connectionString = "server=localhost;database=student;uid=root;pwd=root;";
+        private MySqlConnection _conn;
+
+        public MySqlConnection connectSql()
+        {
+            Console.WriteLine("Connecting to DB");
+
+            _conn = new MySqlConnection(_connectionString);
+            _conn.Open();
+
+            Console.WriteLine(_conn.State == System.Data.ConnectionState.Open
+                ? "Database connection successful"
+                : "Database Connection Failed");
+
+            return _conn;
+        }
+    }
